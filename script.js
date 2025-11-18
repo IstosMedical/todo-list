5c3820b86084e8a49af08498ad130e30b90ede2e
IstosMedical <mazhar@istos.in>
2025-11-18 16:43:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function initUserSession() {
 
 // 🔹 Categories
 const categories = [
-  { id: "Top", title: "🔹 Top-Priority", color: "#E8F5E9" },
+  { id: "Top-Priority", title: "🔹 Top-Priority", color: "#E8F5E9" },
   { id: "leads", title: "🎯 Leads", color: "#FCE4EC" },
   { id: "office", title: "🏢 Office", color: "#FFFDE7" },
   { id: "order", title: "🚚 Orders", color: "#E3F2FD" },
