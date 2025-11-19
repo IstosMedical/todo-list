531e3d5f8c9463597439ba238279aca887cca0e1
IstosMedical <mazhar@istos.in>
2025-11-19 10:52:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -214,7 +214,7 @@ function showToast() {
   toast.classList.remove('toast-hidden');
   setTimeout(() => {
     toast.classList.add('toast-hidden');
-  }, 3200); // Toast visible for 3.2 seconds
+  }, 5200); // Toast visible for 3.2 seconds
 }
 
 // In updateTaskCount()
@@ -259,7 +259,7 @@ function showTaskRain(categoryId) {
   // Remove rain after animation is done
   setTimeout(() => {
     if (rainContainer) rainContainer.innerHTML = '';
-  }, 1700);
+  }, 10000);
 }
 
 // 🔹 User Session
